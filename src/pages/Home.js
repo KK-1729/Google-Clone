5df33d48ac51747f7b1637fb2dcb6562b7b03976
Karthik <kkarthikmadduri1729@gmail.com>
2021-05-27 00:50:12 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+import Search from '../components/Search';
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home_header'>
+                <div className='home_headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className='home_headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+                </div>
+            </div>
+
+            <div className='home_body'>
+                <img src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' alt='pic' />
+                <Search />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
